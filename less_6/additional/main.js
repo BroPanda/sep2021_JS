1f906bd027e061007135797c6be99571e429a70c
BroPanda <juniorik85@gmail.com>
2021-11-25 16:55:13 +0200

    less 6 - v2.4

--- less_6/additional/main.js
+++ less_6/additional/main.js
@@ -11,91 +11,93 @@
 // Примітка
 // Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
 
-// let email = 'some.email@gmail.ua'
+// let email = 'some.email@aa.ua'
 // let validEmail = (mail) => {
+//
 //     if (mail.includes('@', 3)) {
 //         let indexOf = mail.indexOf('@')
-//         if (mail.includes('.', indexOf + 1)) {
-//             let indexOfDot = mail.indexOf('.', indexOf + 1)
+//         if (mail.includes('.', indexOf + 3)) {
+//             let indexOfDot = mail.indexOf('.', indexOf + 3)
 //             if (mail[indexOfDot + 2]) {
 //                 return 'valid'
 //             } else return 'not valid'
 //         } else return 'not valid'
 //     } else return 'not valid'
 // }
+//
 // console.log(validEmail(email));
 
 
 // - є масив
-let coursesArray = [
-    {
-        title: 'JavaScript Complex',
-        monthDuration: 5,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-    },
-    {
-        title: 'Java Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'angular',
-            'aws',
-            'docker',
-            'git',
-            'java core',
-            'java advanced']
-    },
-    {
-        title: 'Python Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'angular',
-            'aws',
-            'docker',
-            'python core',
-            'python advanced']
-    },
-    {
-        title: 'QA Complex',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-    },
-    {
-        title: 'FullStack',
-        monthDuration: 7,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'react',
-            'angular',
-            'aws',
-            'docker',
-            'git',
-            'node.js',
-            'python',
-            'java']
-    },
-    {
-        title: 'Frontend',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
-    }
-];
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
 // відсортувати його в спадаючому порядку за кількістю елементів в полі modules
 
 // let sortArr = coursesArray.sort((a, b) => {
@@ -103,10 +105,42 @@ let coursesArray = [
 // })
 // console.log(sortArr);
 
+
 // - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
 //     let symb = "о", str = "Астрономия это наука о небесных объектах";
 // document.writeln(count(str, symb)) // 5
+
+// let symb = "о"
+// let str = "Астрономия это наука о небесных объектах";
 //
+//
+// let count = (str, stringsearch) => {
+//     let count = 0;
+//     let arr = str.split('')
+//     for (const char of arr) {
+//         if (char === stringsearch) {
+//             count += 1
+//         }
+//     }
+//     return count
+// }
+//
+// console.log(count(str, symb));
+
+
 // - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
 //     let str = "Сила тяжести приложена к центру масс тела";
 // document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+
+// let str = "Сила тяжести приложена к центру масс тела"
+//
+// let cutString = (str, n) => {
+//     let newSrt = str.split(' ').slice(0, n);
+//     return newSrt
+// }
+// console.log(cutString(str, 5));
+
+
+
+
+
