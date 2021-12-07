b8485fcfcde67981f3c2b44bd3c5e861adf314b8
BroPanda <juniorik85@gmail.com>
2021-12-07 11:56:59 +0200

    less 10 - v1.0.1

--- less_10/add/main.js
+++ less_10/add/main.js
@@ -13,40 +13,78 @@
 //     При лівому кліку миші  зробить popup (спливаючий блок) в якому буде вся інформація про блок.
 //     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
 
-onclick = function (e) {
-    let popupClass = document.getElementsByClassName('popup')[0]
-    let popupText = document.getElementsByClassName('popup_text')[0]
-    popupClass.style.opacity = '1';
-    popupClass.style.visibility = 'visible';
-
-    popupText.innerText = `element: ${e.target.localName}
-     have classes: ${e.target.className}
-     id: ${e.target.id},
-     have size h: ${e.target.clientHeight}px w: ${e.target.clientWidth}px`
-}
+// onclick = function (e) {
+//     let popupClass = document.getElementsByClassName('popup')[0]
+//     let popupText = document.getElementsByClassName('popup_text')[0]
+//     popupClass.style.opacity = '1';
+//     popupClass.style.visibility = 'visible';
+//
+//     popupText.innerText = `element: ${e.target.localName}
+//      have classes: ${e.target.className}
+//      id: ${e.target.id},
+//      have size h: ${e.target.clientHeight}px w: ${e.target.clientWidth}px`
+// }
 
 
 // -- взять массив пользователей
-// let usersWithAddress = [
-//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-// ];
-// - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+let usersWithAddress = [
+    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+];
+
+
+
+
+// - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать
+// как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
-//
-//
+
+let divUsers = document.createElement('div')
+
+let checkBox1 = document.createElement('input')
+checkBox1.setAttribute('type', 'checkbox')
+
+let checkBox2 = document.createElement('input')
+checkBox2.setAttribute('type', 'checkbox')
+
+let checkBox3 = document.createElement('input')
+checkBox3.setAttribute('type', 'checkbox')
+
+divUsers.append(checkBox1, checkBox2, checkBox3)
+document.body.append(divUsers)
+
+console.log(checkBox1);
+
+let div = document.createElement('div')
+
+onclick = function (){
+    users = usersWithAddress.filter(value =>
+        value.status === true && checkBox1.checked ||
+        value.age >= 29 && checkBox2.checked ||
+        value.address.city === 'Kyiv' && checkBox3.checked)
+
+    div.innerText = ''
+    for (const user of users) {
+        div.innerText += `${user.id} ${user.name} ${user.age} ${user.status} ${user.address.city}`
+    }
+    divUsers.append(div)
+}
+
+
+
+
 //
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
 // при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
